d1b95a341b3f70f8409228af539ab2469e8d0e8e
phoenix <phoenix-85@github.com>
2026-07-07 19:03:14 +0500

    step 10

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,10 @@
+import formatJSON from './json.js';
 import formatPlain from './plain.js'
 import formatStylish from './stylish.js';
 export default (diff, format) => {
   switch (format) {
+    case 'json':
+      return formatJSON(diff);
     case 'plain':
       return formatPlain(diff);
     default:
